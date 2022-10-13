9feb2a3cb211b85c5d618391af05b7b02d6bbedc
keita080315 <keita080315@gmail.com>
2022-10-13 21:15:25 +0900

    vue.js導入

---
+++ src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Game from "../views/Game.vue";
+import Result from "../views/Result.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/game",
+      name: "game",
+      component: Game,
+    },
+    {
+      path: "/result",
+      name: "result",
+      component: Result,
+    },
+  ],
+});
+
+export default router;
